59feef38f507e7860e24f1784b3aa47c3b821007
brenbread <santos.brenj@gmail.com>
2023-06-05 23:46:25 -0700

    Add toggle functionality for blinking LED effect
    * Defaults to MO(1) on the Scroll Lock position
    * SCRL_FX Keycode, needs to be manually added to VIA JSON file and
      loaded to use

--- keyboards/cannonkeys/cloudline/keymaps/via/keymap.c
+++ keyboards/cannonkeys/cloudline/keymaps/via/keymap.c
@@ -22,10 +22,48 @@ enum layer_names {
     _FN3
 };
 
+enum scroll_lock_keycodes {
+    SCRL_FX
+};
+
+/* If enabled, allows the Scroll Lock LED to blink with each keypress. */
+/* When Scroll Lock is enabled, the LED stays on and does not blink.  */
+#ifdef SCROLL_LOCK_BLINK_EFFECT
+bool BLINK_EFFECT_STATE = true;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    bool blink_state = BLINK_EFFECT_STATE;
+    switch (keycode) {
+        case SCRL_FX:
+            if (record->event.pressed) {
+                BLINK_EFFECT_STATE = !blink_state;
+            }
+            return true;
+        default:
+            if (BLINK_EFFECT_STATE) {
+                bool scroll_state = host_keyboard_led_state().scroll_lock;
+                switch (scroll_state) {
+                    case 0:
+                        if (record->event.pressed) {
+                            writePin(F0, 0);
+                        } else {
+                            writePin(F0, 1);
+                        }
+                        break;
+                    case 1:
+                        writePin(F0, !scroll_state);
+                        break;
+                }
+            }
+            return true;
+    }
+}
+#endif
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_all(
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_MPLY,               KC_PSCR, KC_SCRL, KC_PAUS,
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  MO(1),               KC_PSCR, KC_SCRL, KC_PAUS,
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,      KC_INS,  KC_HOME, KC_PGUP,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,               KC_DEL,  KC_END,  KC_PGDN,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
@@ -33,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                                      KC_RALT, KC_RGUI, KC_RCTL,               KC_LEFT, KC_DOWN, KC_RGHT),
 
   [_FN1] = LAYOUT_all(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, KC_TRNS,               BL_TOGG, BL_DOWN, BL_UP, 
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, SCRL_FX, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_VOLU,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_MPLY, KC_MNXT, KC_VOLD,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
